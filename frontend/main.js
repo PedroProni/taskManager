2faf3b7f4450eb068fa60990d3b269702ab06ab1
Pedro Proni <pedro_proni@hotmail.com>
2025-02-10 11:55:28 -0300

    redirect to /login

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,14 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import User from "./modules/User";
 import Task from "./modules/Task";
+import Auth from "./services/Auth";
 
-const register = new User(".register-form");
-const login = new User(".login-form");
-const task = new Task(".task-form");
+if (Auth.checkAuthAndRedirect()) {
+  const register = new User(".register-form");
+  const login = new User(".login-form");
+  const task = new Task(".task-form");
 
-register.execute();
-login.execute();
-task.execute();
+  register.execute();
+  login.execute();
+  task.execute();
+}
--- frontend/services/Auth.js
+++ frontend/services/Auth.js
@@ -14,6 +14,27 @@ class Auth {
   static isAuthenticated() {
     return !!this.getToken();
   }
+
+  static redirectToLogin() {
+    window.location.replace('/login');
+  }
+
+  static redirectToHome() {
+    window.location.replace('/');
+  }
+
+  static checkAuthAndRedirect() {
+    const isAuthenticated = this.isAuthenticated();
+    const isLoginPage = window.location.pathname === '/login';
+    const isRegisterPage = window.location.pathname === '/register';
+
+    if (!isAuthenticated && !isLoginPage && !isRegisterPage) {
+      this.redirectToLogin();
+      return false;
+    }
+
+    return true;
+  }
 }
 
 export default Auth;
